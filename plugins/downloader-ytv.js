bdd965c084d7fafcf2b53b6f75fa757c23ed0db9
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-04-20 05:04:59 +0700

    Update downloader-ytv.js

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -28,6 +28,7 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
 handler.help = ['ytmp4']
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
+handler.limit = true
 
 module.exports = handler
 
