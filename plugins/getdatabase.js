b3ca7475707498c0774e1a029fbd9621ee741c64
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-04-20 05:07:37 +0700

    Update getdatabase.js

--- plugins/getdatabase.js
+++ plugins/getdatabase.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m, { conn, text }) => {
     m.reply('Tunggu Sebentar, Proses Getting File database.json')
-    let sesi = await fs.readFileSync('./database.json')
+    let sesi = await fs.readFileSync('./rasel.dbjson')
     return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'database.json' }, { quoted: m })
 }
 handler.help = ['getdatabase']
