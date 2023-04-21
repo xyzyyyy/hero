c871b4695d5ece1a257dccae7d40787b2b830834
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-04-21 08:37:34 +0700

    Update getdatabase.js

--- plugins/getdatabase.js
+++ plugins/getdatabase.js
@@ -1,12 +1,12 @@
 let fs = require('fs')
 let handler = async (m, { conn, text }) => {
     m.reply('Tunggu Sebentar, Proses Getting File database.json')
-    let sesi = await fs.readFileSync('./rasel.dbjson')
+    let sesi = await fs.readFileSync('./rasel.db.json')
     return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'database.json' }, { quoted: m })
 }
 handler.help = ['getdatabase']
 handler.tags = ['owner']
-handler.command = /^(getdatabase)$/i
+handler.command = /^(getdb)$/i
 
 handler.owner = true
 
