81566d37ce42cfa064c9ce4ba46bf62dd5d930d2
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-05-09 18:04:44 +0800

    update sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -1,25 +1,23 @@
 let handler = async (m, { conn, text }) => {
+    if (m.quoted && m.quoted.text) text = m.quoted.text;
+    var pp;
+    try {
+        pp = await conn.profilePictureUrl(m.quoted ? m.quoted.sender : m.sender)
+    } catch {
+        throw `PP KOSONG DILARANG MENGGUNAKAN FITUR INI!`
+    }
+    let name = m.quoted ? await conn.getName(m.quoted.sender) : await conn.getName(m.sender)
 
-  if (!text) throw 'Masukan Teks nya!'
+    if (!text) throw 'Masukan Teks nya!'
+    conn.sendFile(m.chat, global.API('https://sh.xznsenpai.xyz', '/api/fakechat', {
+      text: text,
+      username: name,
+      avatar: pp
+    }), 'yt-comment.png', 'ini', m)  
+  }
 
-  conn.sendFile(m.chat, global.API('https://sh.xznsenpai.xyz', '/api/fakechat', {
-
-    text: text,
-
-    username: conn.getName(m.sender),
-
-    avatar: await conn.profilePictureUrl(m.sender).catch(_ => '')
-
-  }), 'yt-comment.png', 'ini', m)
-
-}
-
-handler.help = ['qc']
-
+handler.help = ['qc <text atau reply sebuah message>']
 handler.tags = ['sticker']
-
 handler.command = /^(qc)$/i
 
 module.exports = handler
-
-// https://sh.xznsenpai.xyz/api/fakechat?text=&username=&avatar=
