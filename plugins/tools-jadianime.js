86b60af2d49299e746e047c53f8f07de2da422e4
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-05-15 13:13:22 +0700

    Create tools-jadianime.js

---
+++ plugins/tools-jadianime.js
@@ -0,0 +1,35 @@
+let axios = require('axios')
+
+let { webp2png } = require('../lib/webp2mp4.js')
+
+let handler = async (m, ctx) => {
+
+	try {		let q = m.quoted ? m.quoted : m,
+
+			mime = (q.msg || q).mimetype || ''
+
+		if (/image/g.test(mime)) {
+
+			await m.reply('_In progress, please wait..._')
+
+			let img = await webp2png(await q.download())
+
+			await conn.sendFile(m.chat, `https://xznsenpai.xyz/api/toanime?url=${img}`, '.jpg', 'Success\n\njngn lupa dukung bot ini https://saweria.co/rahardiyan', m)
+
+		} else throw `kirim / reply gambar`
+
+	} catch (e) {
+
+		throw JSON.stringify(e)
+
+	}
+
+}
+
+handler.help = ['jadianime']
+
+handler.tags = ['tools']
+
+handler.command = /^jadianime|toanime$/i
+
+module.exports = handler
