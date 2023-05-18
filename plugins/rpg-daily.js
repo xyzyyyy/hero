043d9a069702a662e7775bd2ead3ad2983448cc7
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-05-18 14:43:08 +0800

    update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -7,14 +7,14 @@ const rewards = {
 const cooldown = 86400000
 let handler = async (m,{ conn} ) => {
   let user = global.db.data.users[m.sender]
-  if (new Date - user.lastclaim < cooldown) throw `You have already claimed this daily claim!, wait for *${((user.lastclaim + cooldown) - new Date()).toTimeString()}*`
+  if (new Date - user.lastclaim < cooldown) throw `anda sudah claim harian anda, mohon tunggu selama *${((user.lastclaim + cooldown) - new Date()).toTimeString()}*`
   let text = ''
   for (let reward of Object.keys(rewards)) {
     if (!(reward in user)) continue
     user[reward] += rewards[reward]
     text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
   }
-  conn.sendButton(m.chat,'*––––––『 DAILY 』––––––*', text.trim(), null, [['Inventory', '.inv'], ['Weekly', '.weekly']],m)
+  conn.sendMessage(m.chat, text.trim(), m)
   user.lastclaim = new Date * 1
 }
 handler.help = ['daily', 'claim']
