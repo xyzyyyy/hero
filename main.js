ba008244f323411b815a8efcf18a41989a5b3f5d
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-05-18 09:15:20 +0800

    update main.js

--- main.js
+++ main.js
@@ -29,6 +29,9 @@ simple.protoType()
 global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in set.api.name.s ? set.api.name.s[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: set.api.key.s[name in set.api.name.s ? set.api.name.s[name] : name] } : {}) })) : '')
 global.set.timestamp = { start: new Date }
 global.db = new Low(new mongoDB('mongodb+srv://ryhar:vtczda83UNH9vVwS@cluster0.xv0opme.mongodb.net/?retryWrites=true&w=majority'))
+global.DATABASE = global.db // mencoba tmbhin ini
+/* new mongoDB(`${set.opts._[0] ? set.opts._[0] + '_' : 'rahardiyan'}.db.json`) // yang ini apakah berguna juga? */
+
 global.loadDatabase = async function loadDatabase() {
   if (global.db.READ) return new Promise((resolve) => setInterval(function () { (!global.db.READ ? (clearInterval(this), resolve(global.db.data == null ? global.loadDatabase() : global.db.data)) : null) }, 1 * 1000))
   if (global.db.data !== null) return
