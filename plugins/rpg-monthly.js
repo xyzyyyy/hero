cc018b45df15a1b7dd9ed2651c7995fcf54de1a0
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-05-18 14:54:33 +0800

    update rpg-monthly.js

--- plugins/rpg-monthly.js
+++ plugins/rpg-monthly.js
@@ -9,13 +9,13 @@ const rewards = {
 const cooldown = 2592000000
 let handler = async (m) => {
     let user = global.db.data.users[m.sender]
-    if (new Date - user.lastmonthly < cooldown) throw `You have already claimed this monthly claim, wait for *${((user.lastmonthly + cooldown) - new Date()).toTimeString()}*`
+    if (new Date - user.lastmonthly < cooldown) throw `anda sudah claim bulanan anda, mohon tunggu selama *${((user.lastmonthly + cooldown) - new Date()).toTimeString()}*`
     let text = ''
     for (let reward of Object.keys(rewards)) if (reward in user) {
         user[reward] += rewards[reward]
         text += `*+${rewards[reward]}* ${rpg.emoticon(reward)}${reward}\n`
     }
-    conn.sendButton(m.chat,'*––––––『 MONTHLY 』––––––*', text.trim(), null, [['Inventory', '.inv'], ['Menu', '.menu']],m)
+    conn.sendButton(m.chat, text.trim(), m)
     user.lastmonthly = new Date * 1
 }
 handler.help = ['monthly']
