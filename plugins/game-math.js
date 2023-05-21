00c4bf4e0b63c2a53bb3e92024895d896246fa6c
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-05-21 18:56:10 +0800

    update game-math.js

--- plugins/game-math.js
+++ plugins/game-math.js
@@ -1,24 +1,24 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn, args, usedPrefix }) => {
   conn.math = conn.math ? conn.math : {}
+  if (args.length < 1) throw `
+Mode: ${Object.keys(modes).join(' | ')}
+
+Contoh penggunaan: ${usedPrefix}math medium
+`.trim()
+  let mode = args[0].toLowerCase()
+  if (!(mode in modes)) throw `
+Mode: ${Object.keys(modes).join(' | ')}
+
+Contoh penggunaan: ${usedPrefix}math medium
+`.trim()
   let id = m.chat
   if (id in conn.math) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.math[id][0])
-  let mode = args[0] ? args[0].toLowerCase() : ''
-  let jsonMode = Object.keys(modes)
-  let row = []
-  for (let modenya of jsonMode) {
-     row.push({
-       title: 'Math ' + modenya.capitalize(),
-       rowId: `${usedPrefix + command} ${modenya}`
-     })
-  } 
-  if (args.lenth < 1) return conn.sendListM(m.chat, `${se.sa} M A T H E M A T I C S\n`, `${set.sb} *Example* : ${usedPrefix + command} ${pickRandom(jsonMode)}`, set.wm, row, m)
-  if (!(mode in modes)) return conn.sendListM(m.chat, `${set.sa} M A T H E M A T I C S\n`, `${set.sb} *Example* : ${usedPrefix + command} ${pickRandom(jsonMode)}`, set.wm, row, m)
   let math = genMath(mode)
   conn.math[id] = [
-    await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(0)} detik\nBonus Jawaban Benar: ${math.bonus} XP\n\nKesempatan menjawab 4X`, m),
+    await conn.reply(m.chat, `Berapa hasil dari *${math.str}*?\n\nTimeout: ${(math.time / 1000).toFixed(2)} detik\nBonus Jawaban Benar: ${math.bonus} XP`, m),
     math, 4,
-    setTimeout(async () => {
-      if (conn.math[id]) conn.sendListM(m.chat, `${set.sa} T I M E  O U T\n`, `Waktu habis!\nJawabannya adalah *${math.result}*`, set.wm, row, conn.math[id][0])                       
+    setTimeout(() => {
+      if (conn.math[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah ${math.result}`, conn.math[id][0])
       delete conn.math[id]
     }, math.time)
   ]
@@ -26,17 +26,18 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 handler.help = ['math <mode>']
 handler.tags = ['game']
 handler.command = /^math/i
+
 module.exports = handler
 
 let modes = {
-  noob: [-3, 3, -3, 3, '+-', 15000, 10],
-  easy: [-10, 10, -10, 10, '*/+-', 20000, 40],
-  medium: [-40, 40, -20, 20, '*/+-', 40000, 150],
-  hard: [-100, 100, -70, 70, '*/+-', 60000, 350],
-  extreme: [-999999, 999999, -999999, 999999, '*/', 99999, 9999],
-  impossible: [-99999999999, 99999999999, -99999999999, 999999999999, '*/', 30000, 35000],
-  impossible2: [-999999999999999, 999999999999999, -999, 999, '/', 30000, 50000]
-}
+  noob: [-3, 3,-3, 3, '+-', 15000, 10000],
+  easy: [-10, 10, -10, 10, '*/+-', 20000, 40000],
+  medium: [-40, 40, -20, 20, '*/+-', 40000, 150000],
+  hard: [-100, 100, -70, 70, '*/+-', 60000, 350000],
+  extreme: [-999999, 999999, -999999, 999999, '*/', 99999, 950000],
+  impossible: [-99999999999, 99999999999, -99999999999, 999999999999, '*/', 30000, 2500000],
+  impossible2: [-999999999999999, 999999999999999, -999, 999, '/', 30000, 4000000]
+} 
 
 let operators = {
   '+': '+',
