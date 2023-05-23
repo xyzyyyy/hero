d0a5484595b3d3b2296e98e4ee18379bef7d4b8a
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-05-23 10:37:24 +0800

    update info-delete.js

--- plugins/info-delete.js
+++ plugins/info-delete.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   if (!m.quoted) throw false
- let { chat, fromMe, id, isBaileys } = m.quoted
+ let { chat, fromMe, id, isBaileys, admin } = m.quoted
  if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
  if (admin) throw 'Khusus Admin'
  conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: m.quoted.id, participant: m.quoted.sender } })
