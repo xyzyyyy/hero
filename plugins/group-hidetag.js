55d20c9e70da48586fe4ca9e2858300036f019d4
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-06-02 20:14:00 +0700

    Update group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -1,38 +1,72 @@
 let { generateWAMessageFromContent } = require('baileys')
-let handler = async (m, { conn, text, participants }) => {
+
+let handler = async (m, { conn, text, isAdmin, participants }) => {
+
+  if (!isAdmin) throw "fitur ini khusus admin!"
+
   let home = participants.map(u => u.id)
+
   let q = m.quoted ? m.quoted : m
+
   let c = m.quoted ? m.quoted : m.msg
+
   let msg = await conn.cMod(
+
     m.chat,
+
     await generateWAMessageFromContent(
+
       m.chat,
+
       {
+
         [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+
           text: await c || ''
+
         },
+
         mentions: await home
+
       }, {
+
         quoted: null,
+
         userJid: conn.user.id
+
       }
+
     ),
+
     text || q.text, conn.user.jid,
+
     {
+
       mentions: await home
+
     }
+
   )
+
   await conn.relayMessage(m.chat, msg.message, {
+
     messageId: msg.key.id
+
   })
+
 }
+
 handler.help = ['hidetag', 'pengumuman'].map(v => v + ' [teks]')
-handler.tags = ['group']
+
+handler.tags = ['admin']
+
 handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
 
 handler.group = true
-handler.admin = true
 
 module.exports = handler
 
 // Aguz Familia
+
+
+
