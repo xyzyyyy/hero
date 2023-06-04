909a4c53b04eab1735747b85ac520a429b1522ef
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-06-04 21:20:58 +0800

    update verifikasi.js

--- plugins/verifikasi.js
+++ plugins/verifikasi.js
@@ -12,20 +12,18 @@ let handler = async(m, { conn, text }) => {
     if(conn.sendMail[id] == undefined ) {
 
         conn.reply(m.chat, "waktu habis. silahkan regist ulang!", m)
+        let user = global.db.data.users[m.sender]
+  if (user.registered === true) throw `Anda sudah terdaftar`
+  user.regTime = + new Date
+  user.registered = true
+
 
     } else {
 
         if (text == conn.sendMail[id].otpCode) {
 
-        conn.reply(m.chat, "Registrasi Berhasil\n\nserial number = ${sn}\nSimpan SN mu jangan sampai hilang, akan dibutuhkan pada saat waktu yang tepat", m)
-        let user = global.db.data.users[m.sender]
-  if (user.registered === true) throw `Anda sudah terdaftar`
-  user.name = name.trim()
-  user.age = age
-  user.regTime = + new Date
-  user.registered = true
-  let sn = createHash('md5').update(m.sender).digest('hex')
-
+        conn.reply(m.chat, "Verifikasi Berhasil!!", m)
+        
         delete conn.sendMail[id]
 
         } else conn.reply(m.chat, "gagal silahkan masukkan ulang otp nya", m)
