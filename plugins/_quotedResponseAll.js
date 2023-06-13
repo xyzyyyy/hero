47dfb281c13b43bf8a7730c43a8f9b6edc1705c9
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-06-13 19:29:38 +0800

    update _quotedResponseAll.js

--- plugins/_quotedResponseAll.js
+++ plugins/_quotedResponseAll.js
@@ -116,23 +116,10 @@ handler.before = async function(m, { conn, isOwner }) {
     // menfess
     if (q.endsWith('menfess_')) {
         if (!m.text) return m.reply('Harap gunakan teks untuk membalas pesan rahasia!')
-        m.react('💌')
+        m.react('✅')
         let med = ["https://telegra.ph/file/620bcc109157b23a656f3.jpg", "https://telegra.ph/file/3eb3c21b4d19626332284.jpg"].getRandom()
         let { data } = await conn.getFile(med)
-        conn.reply(m.quoted.mentionedJid[0], `*MENFESSIN!*\n\nBalasan pesan menfess\n\nPesan : ${m.text}\n\n_Gesek pesan ini kekanan untuk mengirim balasan menfess_`, 0, {
-          ephemeralExpiration: 86400,
-          contextInfo: {
-            mentionedJid: [m.sender],
-            externalAdReply: {
-               showAdAttribution: true,
-               title: 'MENFESSIN',
-               body: 'Follow instagram developer bot ツ',
-               thumbnail: data,
-               sourceUrl: 'https://instagram.com/rasel.ganz'
-            }
-          } 
-       })
-    }
+        conn.reply(m.quoted.mentionedJid[0], `*MENFESSIN!*\n\nBalasan pesan menfess\n\nPesan : ${m.text}\n\n_Gesek pesan ini kekanan untuk mengirim balasan menfess_`, contextInfo: { mentionedJid: [m.sender]})
     //=====================================================================================================================//
 }
 handler.exp = 0
