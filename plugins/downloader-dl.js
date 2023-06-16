bc4ca69efc2b4e04fa23ec4ec5ebbfa8ff5a4073
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-06-16 22:12:37 +0800

    update downloader-dl.js

--- plugins/downloader-dl.js
+++ plugins/downloader-dl.js
@@ -2,7 +2,23 @@ let axios = require('axios')
 let ryhar = async (m, {text}) => {
 	if (!text) throw ('Command Ini Multi Downloader Support url: tiktok, instagram, facebook, youtube, twitter_')
 	 let resp = await axios.get(`https://xzn.wtf/api/download?url=${text}&apikey=ryhar`).catch(e => e.response)
-	await m.reply(`_in progress, please wait..._\nJangan Lupa Dukung Bot Ini https://saweria.co/rahardiyan`)
+	const arr = [
+"《 ▒▒▒▒▒▒▒▒▒▒▒▒▒▒》0%",
+"《 ███▒▒▒▒▒▒▒▒▒▒▒》20%",
+"《 ██████▒▒▒▒▒▒▒▒》40%",
+"《 █████████▒▒▒▒▒》60%",
+"《 ████████████▒▒》80%",
+"《 ██████████████》100%",
+  "Done!!",
+  "Mohon Tunggu Bot Sedang Mengirim Video nya\nJangan Lupa Dukung Bot Ini https://saweria.co/rahardiyan"
+  ]
+  
+const { key } = await conn.sendMessage(m.chat, { text: '_in progress, please wait..._' });
+
+for (let i = 0; i < arr.length; i++) {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  await  conn.editMessage(m.chat, key, arr[i])
+}
 	if (resp.status != 200) throw resp.statusText
 	let data = resp.data
 	if (Array.isArray(data)) {
