f9cb52c83a9051d367795bde2c61005d883626bc
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-06-18 09:01:04 +0700

    Update game-skata.js

--- plugins/game-skata.js
+++ plugins/game-skata.js
@@ -99,9 +99,9 @@ Dan hanya bisa dimainkan oleh player yang terdaftar`.trim()
        }
     }
 }
-handler.help = ['sambungkata *BUG*']
+handler.help = ['sambungkata']
 handler.tags = ['game']
-//handler.command = /^s(ambung)?kata(debug)?$/i
+handler.command = /^s(ambung)?kata(debug)?$/i
 handler.group = true
 handler.game = true
 module.exports = handler
