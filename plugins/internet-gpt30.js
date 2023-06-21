61b10b4331b94266cece1b73cffc69568c1bba21
Rahardiyan <107953532+xyzyyyy@users.noreply.github.com>
2023-06-22 00:59:39 +0800

    update internet-gpt30.js

--- plugins/internet-gpt30.js
+++ plugins/internet-gpt30.js
@@ -1,10 +1,8 @@
-const axios = require('axios');
-
 let handler = async(m, { conn, usedPrefix, text }) => {
-    if (!text) throw `Kamu mau nanya apa`
-        const { data } = await axios.get("https://xzn.wtf/api/openai?text=${text}&apikey=ryhar")
-        let hasil = `${data.result}`
-        m.reply(hasil)
+    if (!text) throw `Kamu mau nanya apa?`
+let api = await fetch(`https://xzn.wtf/api/openai?text=${text}&apikey=ryhar`)
+        let hasil = await api.json()
+        m.reply(hasil.result)
         }
 handler.command = /^ai$/i
 handler.help = ["ai"]
